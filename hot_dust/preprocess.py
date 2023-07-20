1e653e20b030b4e5a3587a60d60ef4e40c527fb6
Micah <micahwallace00@gmail.com>
2023-07-20 10:16:07 -0400

    playing with the histogram

--- hot_dust/preprocess.py
+++ hot_dust/preprocess.py
@@ -1,5 +1,6 @@
 import xarray as xr
-import hvplot.xarray
+import hvplot.xarray 
+import numpy as np
 
 def prepare_training_data():
     ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')
@@ -8,4 +9,10 @@ def prepare_training_data():
 
 def feature_histogram(variable):
     plt = variable.hvplot.hist()
+    return plt 
+
+def modified_histogram(variable): # See if the bins will make the data easier to read 
+    n_bins = int(np.ceil(np.sqrt(len(variable))))  
+    plt = plt = variable.hvplot.hist(bins=n_bins)
     return plt
+
