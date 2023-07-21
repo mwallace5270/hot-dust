f43d9ca8012bd1b71078f246cc91013c33f1c66c
Micah <micahwallace00@gmail.com>
2023-07-21 15:58:26 -0400

    commented out heatmap function & calls

--- hot_dust/preprocess.py
+++ hot_dust/preprocess.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import hvplot.xarray 
 import numpy as np 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def prepare_training_data():
     ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')
@@ -12,7 +12,7 @@ def feature_histogram(variable):
     plt = variable.hvplot.hist()
     return plt  
 
-def heat_map(variable): 
+#def heat_map(variable): 
     plt.imshow(variable) 
     plt.show()  
 
