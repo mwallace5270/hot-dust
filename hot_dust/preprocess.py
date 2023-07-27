1c1d2873ab56082dcc9d1f0be73450e2da90ea8f
Micah <micahwallace00@gmail.com>
2023-07-27 18:18:21 -0400

    end of day submit

--- hot_dust/preprocess.py
+++ hot_dust/preprocess.py
@@ -16,18 +16,18 @@ def heat_map(variable):
     plt.imshow(variable) 
     plt.show()  
 
-def split_training_data():  
+def split_training_data():
+    # Random but reproducible data shuffle
     ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')
-    rng = np.random.default_rng(1234)  
-    rng.shuffle(ds)  
-
+    rng = np.random.default_rng(1234)   
+    ds_random = ds.sample(random=rng)
     # Define the percentages of the data to take
     train_ratio = 0.6 
     validate_ratio = 0.1 
     test_ratio = 0.3  
 
     # Get the amount of data to pull into the new dataset
-    ds_len = len(ds) 
+    ds_len = len(ds_random) 
     train_len = train_ratio * ds_len 
     validate_len = validate_ratio * ds_len 
     test_len = test_ratio * ds_len   
