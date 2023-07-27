c98b6045a8a50756739f5a8d49e2baef11c6869d
Micah <micahwallace00@gmail.com>
2023-07-27 14:04:28 -0400

    split training data code

--- hot_dust/preprocess.py
+++ hot_dust/preprocess.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import hvplot.xarray 
 import numpy as np 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def prepare_training_data():
     ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')
@@ -15,4 +15,31 @@ def feature_histogram(variable):
 def heat_map(variable): 
     plt.imshow(variable) 
     plt.show()  
+
+def split_training_data():  
+    ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')
+    rng = np.random.default_rng(1234)  
+    rng.shuffle(ds)  
+
+    # Define the percentages of the data to take
+    train_ratio = 0.6 
+    validate_ratio = 0.1 
+    test_ratio = 0.3  
+
+    # Get the amount of data to pull into the new dataset
+    ds_len = len(ds) 
+    train_len = train_ratio * ds_len 
+    validate_len = validate_ratio * ds_len 
+    test_len = test_ratio * ds_len   
+
+    # Split into train, validate, and test subsets 
+    train_ds = ds[:train_len] # type: ignore
+    validate_ds = ds[:validate_len] # type: ignore
+    test_ds = ds[:test_len]  # type: ignore
+
+    return test_ds
+
+
+
+
 
