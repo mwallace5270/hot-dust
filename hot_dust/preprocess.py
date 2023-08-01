9e53581dcb6558127f1b77bbe898f2f1a3053b86
Micah <micahwallace00@gmail.com>
2023-08-01 16:16:21 -0400

    finished debugging and displayed test preview

--- hot_dust/preprocess.py
+++ hot_dust/preprocess.py
@@ -20,16 +20,18 @@ def heat_map(variable):
     plt.show()  
 
 def split_training_data():
-    ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')  
-    
+    ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')   
+        
     # Convert xarray to numpy array for shuffling   
-    ds_np = ds.values
-    
+    ds_np = ds.to_array().values
+
     # Random but reproducible data shuffle
     rng = np.random.default_rng(seed=1234)    
-    ds_random = rng.shuffle(ds_np, axis=0) 
+    ds_random = rng.permutation(ds_np) # .permutation instead of .shuffle 
 
-    
+    # Reshape to remove the extra dimension
+    ds_random = ds_random.reshape(-1, ds_random.shape[-1]) 
+
     # Define the percentages of the data to take
     train_ratio = 0.6 
     validate_ratio = 0.1 
@@ -39,7 +41,7 @@ def split_training_data():
     ds_len = len(ds_random)  
     train_len = int(train_ratio * ds_len)
     validate_len = int(validate_ratio * ds_len)
-    test_len = int(test_ratio * ds_len)
+    test_len = int(test_ratio * ds_len) 
 
     # Split into train, validate, and test subsets 
     X_train = ds_random[:train_len, :-1]
@@ -51,17 +53,14 @@ def split_training_data():
     X_test = ds_random[train_len + validate_len: train_len + validate_len + test_len, :-1]
     y_test = ds_random[train_len + validate_len: train_len + validate_len + test_len, -1]
 
-    # Stack all predictors into a 2D array   
-    X_train =  np.vstack(X_train)
-    X_validate = np.vstack(X_validate) 
-    X_test = np.vstack(X_test)
-    
     # Return the data splits as tf.data.Dataset constructed from tf.data.Dataset.from_tensor_slices 
-    train_dataset = tf.data.Dataset.from_tensor_slices(X_train, y_train) 
-    validate_dataset = tf.data.Dataset.from_tensor_slices(X_validate, y_validate) 
-    test_dataset = tf.data.Dataset.from_tensor_slices(X_test, y_test)  
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)) 
+    validate_dataset = tf.data.Dataset.from_tensor_slices((X_validate, y_validate))
+    test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+    
+    return X_test, y_test
+    #return train_dataset, validate_dataset, test_dataset
 
-    return train_dataset
 
     
 
