68034f8c2ed003785aeea7044f93e0ce02347806
Ian Carroll <ian.t.carroll@nasa.gov>
2023-08-02 12:07:49 -0400

    working fit.ipynb, but broken preview.ipynb

--- hot_dust/preprocess.py
+++ hot_dust/preprocess.py
@@ -1,70 +1,81 @@
 import xarray as xr
-import hvplot.xarray 
-import numpy as np 
-import matplotlib.pyplot as plt 
-import tensorflow as tf 
+import hvplot.xarray
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
 def prepare_training_data():
-    ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')
-    return ds
+    # Read the training data file
+    ds = xr.open_dataset("data/rt_nn_irdust_training_data.nc")
+
+    # Flatten multi-dimensional features (only viirs_bts)
+    viirs_bts_labels = [f"bt_{int(i)}" for i in ds["nbands_viirs"].data]
+    ds["nbands_viirs"] = viirs_bts_labels
+    viirs_bts = ds["viirs_bts"].to_dataset(dim="nbands_viirs")
+    ds = xr.merge((ds, viirs_bts))
+
+    # Combine predictors into "x"
+    features = [
+        "solar_zenith_angle",
+        "viewing_zenith_angle",
+        "relative_azimuth_angle",
+        "spress",
+        "h2o",
+        "o3",
+        "ws",
+        "ts",
+        *viirs_bts_labels,
+    ]
+    ds["x"] = xr.concat(
+        [ds[i] for i in features],
+        dim=xr.IndexVariable("features", features),
+    )
+    ds["x"].attrs = {}
+
+    # Assign response (as the log of dust optical thickness) to "y"
+    ds["y"] = np.log(ds["dust_optical_thickness"])
+    ds["y"].attrs = {}
+
+    # Return just "x" and "y" with "sample" as the first dimension
+    return ds[["x", "y"]].rename({"npoints": "sample"}).transpose("sample", ...)
+
 
 def feature_histogram(variable):
     plt = variable.hvplot.hist()
-    return plt  
-
-def heat_map(variable): 
-   plt = variable.hvplot.heatmap() 
-   return plt
-
-def split_training_data():
-    ds = xr.open_dataset('data/rt_nn_irdust_training_data.nc')   
-        
-    # Convert xarray to numpy array for shuffling   
-    ds_np = ds.to_array().values
-
-    # Random but reproducible data shuffle
-    rng = np.random.default_rng(seed=1234)    
-    ds_random = rng.permutation(ds_np) # .permutation instead of .shuffle 
-
-    # Reshape to remove the extra dimension
-    ds_random = ds_random.reshape(-1, ds_random.shape[-1]) # The first dimesion made the shape odd
-
-    # Define the percentages of the data to take
-    train_ratio = 0.6 
-    validate_ratio = 0.1 
-    test_ratio = 0.3  
-
-    # Get the amount of data to pull into the new dataset
-    ds_len = len(ds_random)  
-    train_len = int(train_ratio * ds_len)
-    validate_len = int(validate_ratio * ds_len)
-    test_len = int(test_ratio * ds_len) 
-
-    # Split into train, validate, and test subsets 
-    X_train = ds_random[:train_len, :-1]
-    y_train = ds_random[:train_len, -1]
-
-    X_validate = ds_random[train_len:train_len + validate_len, :-1]
-    y_validate = ds_random[train_len:train_len + validate_len, -1]
-
-    X_test = ds_random[train_len + validate_len: train_len + validate_len + test_len, :-1]
-    y_test = ds_random[train_len + validate_len: train_len + validate_len + test_len, -1]
-
-    # Return the data splits as tf.data.Dataset constructed from tf.data.Dataset.from_tensor_slices 
-    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)) 
-    validate_dataset = tf.data.Dataset.from_tensor_slices((X_validate, y_validate))
-    test_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test))
-    
-    return X_test, y_test
-    #return train_dataset, validate_dataset, test_dataset
+    return plt
 
 
-    
+def heat_map(variable):
+    plt = variable.hvplot.heatmap()
+    return plt
 
 
+def split_training_data(ds: xr.Dataset):
+    # Assign a split variable with a random but reproducible category
+    rng = np.random.default_rng(seed=1234)
+    ds["split"] = (
+        # dimensions
+        "sample",
+        # values
+        rng.choice(
+            ["train", "validate", "test"],
+            size=ds.sizes["sample"],
+            p=[0.8, 0.15, 0.05],
+        ),
+    )
+
+    # Split into train, validate, and test subsets
+    ds = {key: value.drop_vars("split") for key, value in ds.groupby("split")}
+
+    # Return the three datasets
+    return ds["train"], ds["validate"], ds["test"]
 
 
-
-
-
+def to_tensorflow(ds):
+    dataset = []
+    for item in ds:
+        dataset.append(
+            tf.data.Dataset.from_tensor_slices((item["x"].values, item["y"].values))
+        )
+    return tuple(dataset)
